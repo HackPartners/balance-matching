879d5d2785cddf381ae98f26946eb74983abb756
Seb <skay914@googlemail.com>
2024-04-29 16:22:08 +0100

    test: added test

---
+++ tests/test.py
@@ -0,0 +1,33 @@
+import json
+import os
+import warnings
+
+# import curlify
+
+import logging
+import requests
+
+
+def main():
+
+
+    # Local URL
+    url = "http://localhost:4000"
+    # scan_name = "ANG2304_00051"
+
+    send_data = {"match": True}
+
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url+"/post/json/", headers=headers, data=json.dumps(send_data))
+
+    # r is the response object from the requests library.
+    # print(curlify.to_curl(requests.post(url+"/post/json/", headers=headers, data=json.dumps(send_data)).request))
+
+    # Check response:
+    if response.status_code == 200:
+        print(f"Have a response. 200")
+    else:
+        print(f"Bad respons code: {response.status_code}")
+
+if __name__ == "__main__":
+    main()
